b28265c6be2cca9743f79806bd4ed395765ad549
virgilvox <moheeb.zara@citrix.com>
2016-01-20 17:28:29 -0700

    v1.0.27 ugh

--- drive_prod.js
+++ drive_prod.js
@@ -64,7 +64,7 @@ conn.on('ready', function(data){
       fps: 100,          // Used to calculate rate of movement between positions
       invert: false,     // Invert all specified positions
       startAt: 90,       // Immediately move to a degree
-      center: true,      // overrides startAt if true and moves the servo to the center of the range
+      center: false,      // overrides startAt if true and moves the servo to the center of the range
       specs: {           // Is it running at 5V or 3.3V?
         speed: five.Servo.Continuous.speeds["@5.0V"]
       }
